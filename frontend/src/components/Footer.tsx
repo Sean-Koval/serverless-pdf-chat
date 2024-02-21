d453577a6bf1431537611d84c768c78e62d215f7
Sean Koval <sean.koval@symphony.com>
2024-02-21 10:14:29 -0500

    improved footer ui

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -8,16 +8,23 @@ const Footer: React.FC = () => {
       <footer className="container">
         <div className=" flex flex-row justify-between py-3 text-sm">
           <div className="inline-flex items-center">
-            <CloudIcon className="w-5 h-5 mr-1.5" />
-            Symphony Document Chat - Powered by AWS
-          </div>
-          <div className="inline-flex items-center hover:underline underline-offset-2">
             <img
               src={Symphony}
               alt="React Logo"
               width={20}
               className="mr-1.5 py-2 mx-2"
             />
+            <span>Symphony Document Chat</span>
+            {/* <CloudIcon className="w-5 h-5 mr-1.5" />
+            Symphony Document Chat - Powered by AWS */}
+          </div>
+          <div className="inline-flex items-center hover:underline underline-offset-2">
+            {/* <img
+              src={Symphony}
+              alt="React Logo"
+              width={20}
+              className="mr-1.5 py-2 mx-2"
+            /> */}
           </div>
         </div>
       </footer>
